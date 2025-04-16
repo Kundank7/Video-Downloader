5cf9411c7e1780def726ee54f338a20ba7fa466b
Kundank7 <149052665+Kundank7@users.noreply.github.com>
2025-04-16 19:54:42 +0530

    app.py

--- app.py
+++ app.py
@@ -80,4 +80,5 @@ def get_file(filename):
     )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
